5cb1442b822ec6183678cff6556c481ddabe4ce9
CatsAreMLG <joshuadgon@gmail.com>
2019-07-05 19:26:04 -0500

    Add red damage filter to entities

--- engine/entity.gd
+++ engine/entity.gd
@@ -10,7 +10,14 @@ var knockdir = dir.center
 var spritedir = "right"
 
 var hitstun = 0
+var hurt = 0
 var health = 1
+var texture_default = null
+var texture_hurt = null
+
+#func _ready():
+#	texture_default =$Sprite.texture
+#	texture_hurt = load($Sprite.texture.get_path().replace(".png", "_hurt.png"))
 
 func movement_loop():
 	var motion
@@ -35,12 +42,21 @@ func anim_switch(animation):
 func damage_loop():
 	if hitstun > 0:
 		hitstun -= 1
+	if hurt > 0:
+		hurt -= 1
+		#To make sprite turn to white
+		$Sprite.modulate = Color(10,0.5,0.5,0.9)
+	else:
+		#To make sprite turn to white
+		$Sprite.modulate = Color(1,1,1,1)
 	for area in $hitbox.get_overlapping_areas():
 		var body = area.get_parent()
 		if hitstun == 0 && body.get("DAMAGE") != 0 && body.get("TYPE") != TYPE:
 			health -= body.get("DAMAGE")
 			hitstun = 10
 			knockdir = global_transform.origin - body.global_transform.origin
+			if hurt == 0:
+				hurt = 5
 
 func use_item(item):
 	var newitem = item.instance()
